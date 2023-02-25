a7db79cdb47f742508b6cb937437c5f951deb7de
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-02-25 20:03:57 +0300

    Create DirectoryModel.java

---
+++ phone_directory/DirectoryModel.java
@@ -0,0 +1,13 @@
+class DirectoryModel {
+    String name;
+    String phNum;
+    String email;
+
+    DirectoryModel() {}
+
+    DirectoryModel(String name, String phNum, String email) {
+        this.name = name;
+        this.phNum = phNum;
+        this.email = email;
+    }
+}
